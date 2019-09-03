bfd07c5ca5a7d1d2f4bc76a965f870a12b08d26c
Xiomara <xiomara.torres@tecsup.edu.pe>
2019-09-03 17:52:41 -0500

    Cambios 4

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -1,5 +1,17 @@
 package com.tecsup.lab01.controller;
 
 public class UserController {
-    
+    user controller:
+package com.tecsup.lab01.controller;
+
+import com.tecsup.lab01.model.User;
+
+public class UserController{
+   User u = new User();
+  
+   String nom = u.nombre;
+   String ape = u.apellido;
+   int age = u.edad;
+  
+}
 }
--- src/main/java/com/tecsup/lab01/model/User.java
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -1,5 +1,41 @@
 package com.tecsup.lab01.model;
 
 public class User {
+    public String nombre;
+   public String apellido;
+   public int edad;
 
+   public User(String nombre, String apellido, int edad) {
+       this.nombre = nombre;
+       this.apellido = apellido;
+       this.edad = edad;
+   }
+
+   public User() {
+       throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   }
+
+   public String getNombre() {
+       return nombre;
+   }
+
+   public void setNombre(String nombre) {
+       this.nombre = nombre;
+   }
+
+   public String getApellido() {
+       return apellido;
+   }
+
+   public void setApellido(String apellido) {
+       this.apellido = apellido;
+   }
+
+   public int getEdad() {
+       return edad;
+   }
+
+   public void setEdad(int edad) {
+       this.edad = edad;
+   }
 }
